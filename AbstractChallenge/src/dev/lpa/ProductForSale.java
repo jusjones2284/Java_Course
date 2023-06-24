9219a899ac857d0e567d7d598f96071def6ba532
Justin Jones <jus.jones@yahoo.com>
2023-06-24 13:58:51 -0400

    abstract challenge

---
+++ AbstractChallenge/src/dev/lpa/ProductForSale.java
@@ -0,0 +1,32 @@
+package dev.lpa;
+
+public abstract class ProductForSale {
+
+    protected String type;
+    protected double price;
+    protected  String description;
+    //the subclasses can access and modify as well
+
+    //constructor
+
+    public ProductForSale(String type, double price, String description){
+
+        this.type = type;
+        this.price = price;
+        this.description = description;
+
+    }
+
+    public double getSalesPrice(int qty){
+        return qty * price;
+    }
+
+    public void printPricedItem(int qty){
+
+        System.out.printf("%2d qty at $%8.2f each, %-15s %-35s %n",
+                qty, price, type, description);
+    }
+
+    public abstract void showDetails();
+
+}
